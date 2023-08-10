64a7690b260350e1967f0c08ffeb6a3fe4cf6253
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-10 12:18:04 +0900

    [Script push]

---
+++ src/main/kotlin/studio/hcmc/kotlin/coroutines/CoroutineContext.kt
@@ -0,0 +1,16 @@
+package studio.hcmc.kotlin.coroutines
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+fun CoroutineContext.launch(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+): Job {
+    return CoroutineScope(context).launch(context, start, block)
+}
